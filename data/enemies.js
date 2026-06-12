584c0786a16269be0638506a7dfc5fff78a2a0da
pegblineoa <pegblineoa@gmail.com>
2026-06-12 16:23:19 +0700

    logic/bkk; weight public_bus from 3000 to 3

--- data/enemies.js
+++ data/enemies.js
@@ -249,7 +249,7 @@ const ENEMIES_DATA = [
   { // Public Bus (Black Smoke)
     id: 'public_bus',
     dungeonId: 'bangkok_nightmare',
-    weight: 3000,
+    weight: 3,
     name: 'Public Bus No. 8',
     area: 'Bangkok Nightmare',
     hp: 420,
